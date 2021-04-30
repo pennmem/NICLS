5797d0cc5c3d4eaf915e46a11bb226b108498d7c
James Bruska <jamesbthecoder@gmail.com>
2021-04-30 11:07:33 -0400

    Initial attempt at installer

--- setup.py
+++ setup.py
@@ -1,3 +1,27 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='NICLServer',
+    version=0.2.1,
+    maintainer=['James Bruska', 'Joey Rudoler'],
+    maintainer_email=['jbruska@sas.upenn.edu', 'jrudoler@sas.upenn.edu', 'kahana-sysadmin@sas.upenn.edu'],
+    url='https://github.com/pennmem/NICLS',
+    description = ("The backend system used to control the NICLS experiment"),
+    long_description=read('README'),
+
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "django",
+        "aiofiles",
+        "zmq",
+    ],
+    packages=find_packages(
+        exclude=['*.tests', 'tests', 'tests.*', '*.outdated_tests']
+    ),
+)
+
+
